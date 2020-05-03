eb188e95f2cbf41b822f6416d382e12fc9f40799
Denis Hallulli <hallulli.denis@gmail.com>
2020-05-03 04:21:11 +0200

    Added artificial thinking time for bullet games

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import sys, re, time, pyautogui, chess, chess.engine, chess.polyglot, random as rand
+import sys, re, time, pyautogui, chess, chess.engine, chess.polyglot, math, random as rand
 import selenium.webdriver as driver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -16,6 +16,9 @@ class Bot:
         self.moves_game = []
         self.black_entered_match = [False]
         self.cache = -1 
+        self.elapsed_time = 0
+        self.attackers = [0]
+        self.thought_about_mate = False
         self.pos_w = self.__create_dic('w')
         self.pos_b = self.__create_dic('b')
         self.WAIT_TIME = 3600
@@ -70,20 +73,96 @@ class Bot:
             WebDriverWait(browser, self.WAIT_TIME).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='main-wrap']/main/div[1]/div[1]/div/cg-helper/cg-container/cg-board/piece[28]")))
         finally:
             return True
+    
+    def __find_attackers(self, side):
+
+        num_attackers = 0
+
+        if side == 'black':
+            for piece_pos in self.board.piece_map():
+                if str(self.board.piece_at(piece_pos)) in ['p', 'q', 'r', 'n', 'b', 'k']:
+                    num_attackers += len(self.board.attackers(chess.WHITE, piece_pos))
+        
+        elif side == 'white':
+            for piece_pos in self.board.piece_map():
+                if str(self.board.piece_at(piece_pos)) in ['P', 'Q', 'R', 'N', 'B', 'K']:
+                    num_attackers += len(self.board.attackers(chess.BLACK, piece_pos))
+        
+        self.attackers.append(num_attackers)
+        
+    
+    def __move_type(self, side, move_pos, num_moves):
+        if num_moves > 1:
+            self.__find_attackers(side)
+
+            if self.attackers[-1] - self.attackers[-2] > 0:
+                new_attackers = self.attackers[-1] - self.attackers[-2]
+            else:
+                new_attackers = 0
+
+            if re.search(r'x[a-h][1-8]', self.moves_game[-1]) and self.moves_game[-1][2:] == move_pos:
+                    recapture_piece = True
+            else:
+                recapture_piece = False
+                            
+            if re.search(r'Q[a-h][1-8]', self.moves_game[-1]) and self.moves_game[-1][1:] == move_pos:
+                queen_blunder = True
+            else:
+                queen_blunder = False
+
+            return recapture_piece, queen_blunder, new_attackers
+        else:
+            return False, False, False
+    
+    def __wait_time(self, side, score_change, num_until_mate, move_pos):
+
+        if side == 'black':
+            n_moves = self.board.fullmove_number - 1
+        else:
+            n_moves = self.board.fullmove_number
+        
+        quick_moves = [rand.randint(9,35) for _ in range(5)]
+        recapture, Qblunder, n_attacks = self.__move_type(side, move_pos, n_moves)
+
+        if  n_moves < 6 or recapture or Qblunder or self.elapsed_time > 50 or (n_moves in quick_moves) or self.thought_about_mate:
+            return 0
+        elif num_until_mate < 7:
+            if num_until_mate < 3:
+                return 0
+            else:
+                self.thought_about_mate = True
+                return num_until_mate + (num_until_mate - rand.randint(2,4))**2/3-3
+        else:
+            return rand.uniform(0.1,0.3) + 0.3*n_attacks + math.exp((abs(score_change)-7)*(9-abs(score_change))/10)*rand.uniform(4,6)
 
     def  __move_piece(self, engine, book, side):
+
+        score_change = 0
+        moves_mate =  math.inf
+
         if len(self.moves_game) != 0:
             self.board.push_san(self.moves_game[-1])
         
         if sum(1 for _ in book.find_all(self.board)) != 0:
             for entry in book.find_all(self.board):
-                next_move = entry.move  
+                next_move = entry.move
+                self.board.push(next_move)
                 break
         else:
-            engine_move = engine.play(self.board, chess.engine.Limit(time=0.1))
-            next_move = engine_move.move
-
-        self.board.push(next_move)
+            if self.elapsed_time < 50:
+                score_prior = engine.analyse(self.board, chess.engine.Limit(time=0.2))
+                engine_move = engine.play(self.board, chess.engine.Limit(time=0.1))
+                next_move = engine_move.move
+                self.board.push(next_move)
+                score_after = engine.analyse(self.board, chess.engine.Limit(time=0.2))
+                if str(score_after["score"])[0] == '#':
+                    moves_mate = int(str(score_after["score"])[2:])
+                else:
+                    score_change = (int(str(score_after["score"])) - int(str(score_prior["score"])))/100
+            else:
+                engine_move = engine.play(self.board, chess.engine.Limit(time=0.1))
+                next_move = engine_move.move
+                self.board.push(next_move)
 
         start_click = str(next_move)[0:2]
         end_click = str(next_move)[2:4]
@@ -95,6 +174,7 @@ class Bot:
             start_pos = self.pos_b[start_click]
             end_pos = self.pos_b[end_click]
 
+        time.sleep(self.__wait_time(side, score_change, moves_mate, end_click))
         pyautogui.click(start_pos[0], start_pos[1])
         pyautogui.click(end_pos[0], end_pos[1])
 
@@ -123,16 +203,20 @@ class Bot:
         if side == 'black':
             self.__moves_history()
             if len(self.moves_game)%2 != 0 and len(self.moves_game) > self.cache:
+                start_time = time.time()
                 self.cache = len(self.moves_game)
                 self.__move_piece(engine, book, side)
+                self.elapsed_time += time.time() - start_time
 
         elif side == 'white':
             if not self.black_entered_match[-1]:
                 self.black_entered_match.append(self.__load_engine_as_white())
                     
             if len(self.moves_game)%2 == 0 and self.black_entered_match and len(self.moves_game) > self.cache:
+                start_time = time.time()
                 self.cache = len(self.moves_game)
                 self.__move_piece(engine, book, side)
+                self.elapsed_time += time.time() - start_time
             self.__moves_history()
 
 def main():
@@ -141,7 +225,7 @@ def main():
     logged_in = False 
     engine = chess.engine.SimpleEngine.popen_uci("engine")
     book = chess.polyglot.open_reader("lichess_bot.bin")
-    engine.configure({"UCI_Elo": "2000", "Hash": "64"})
+    engine.configure({"UCI_Elo": "1700", "Hash": "64"})
 
     while True:
 
